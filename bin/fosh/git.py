9af67aa32be0d7ad454ac68dc67cefb6b7d8c054
root <root@fo-tp-vh02.(none)>
2010-11-28 09:09:24 +0100

    remote install now works.

--- bin/fosh/git.py
+++ bin/fosh/git.py
@@ -7,3 +7,4 @@ def git_commit(comment):
   general.shell_exec("cd /opt/fosh;git add *")
   general.shell_exec("cd /opt/fosh;git commit -a -m'%s'" % comment)
   general.shell_exec("git push origin")
+  
