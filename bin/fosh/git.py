58700f8a956ac5cddb6bf9f9db97229cc00c4315
root <root@fo-tp-vh02.(none)>
2010-11-28 09:12:11 +0100

    remote install now works.

--- bin/fosh/git.py
+++ bin/fosh/git.py
@@ -3,6 +3,9 @@
 import general
 
 def git_commit(comment):
+  '''
+  Commit the fosh folder to the github repository.
+  '''
   general.shell_exec("find /opt/fosh/ -iname '*.pyc' -exec rm {} \;")
   general.shell_exec("cd /opt/fosh;git add *")
   general.shell_exec("cd /opt/fosh;git commit -a -m'%s'" % comment)
