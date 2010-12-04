8eea82274702163825bfe63d0869d82eab25698d
Daniel Lindh <daniel@cybercow.se>
2010-12-04 01:11:49 +0100

    install-guests update

--- bin/fosh/git.py
+++ bin/fosh/git.py
@@ -1,12 +1,12 @@
 #! /usr/bin/env python
 
-import general
+import general, app
 
 def git_commit(comment):
   '''
   Commit the fosh folder to the github repository.
   '''
-  general.shell_exec("find /opt/fosh/ -iname '*.pyc' -delete")
-  general.shell_exec("cd /opt/fosh;git add *")
-  general.shell_exec("cd /opt/fosh;git commit -a -m'%s'" % comment)
+  general.shell_exec("find " + app.fosh_path + " -iname '*.pyc' -delete")
+  general.shell_exec("cd " + app.fosh_path + ";git add *")
+  general.shell_exec("cd " + app.fosh_path + ";git commit -a -m'%s'" % comment)
   general.shell_exec("git push origin")
