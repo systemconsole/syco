157d704919743f56f13f54232c6abb0fc68158dc
root <root@fo-tp-gf-rd-int.(none)>
2011-01-21 23:09:19 +0100

    Added general iptables rules and specific for glassfish and mysql.

--- bin/fosh/iptables.py
+++ bin/fosh/iptables.py
@@ -1,5 +1,22 @@
 #! /usr/bin/env python
+#
+# Setup an iptable firewall according to the installed services.
+# 
+# If for example mysql installed, port 3306 will be opened for incoming.
+#
+# This script is based on Oskar Andreassons rc.DMZ.firewall.txt
+# Read and learn more about iptables.
+# http://www.frozentux.net/iptables-tutorial/scripts/rc.DMZ.firewall.txt
+# http://www.frozentux.net/iptables-tutorial/iptables-tutorial.html
+# http://manpages.ubuntu.com/manpages/jaunty/man8/iptables.8.html
+# http://www.cipherdyne.org/psad/
+#
+# TODO: 
+# * Add recent http://www.snowman.net/projects/ipt_recent/
+#   http://www.frozentux.net/iptables-tutorial/scripts/recent-match.txt
+# * SSH to the server should only be allowed from certain ips.
 
+import os
 import app, general
 
 # The version of this module, used to prevent
@@ -9,12 +26,14 @@ script_version = 1
 
 def build_commands(commands):
   commands.add("iptables-clear", iptables_clear, help="Clear all rules from iptables.")
+  commands.add("iptables-setup", iptables_setup, help="Setup an iptable firewall, customized for installed services.")
   
 def iptables_clear(args):
   '''
+  Remove all iptables rules.
   
   '''
-  app.print_verbose("Clear all iptables rules")
+  app.print_verbose("Clear all iptables rules.")
   # reset the default policies in the filter table.
   iptables("-P INPUT ACCEPT")
   iptables("-P FORWARD ACCEPT")
@@ -46,6 +65,108 @@ def iptables_clear(args):
   iptables("-Z -t filter")
   iptables("-Z -t nat")
   iptables("-Z -t mangle")
+
+def iptables_setup(args):
+  iptables_clear(args)
+  _deny_all()
+  _create_chains()  
+  _setup_general_rules()  
+  _setup_ssh_rules()
   
+  if (os.access("/usr/bin/mysqld_safe", os.F_OK)):      
+    _setup_mysql_rules()
+
+  if (os.access("/usr/local/glassfish", os.F_OK)):      
+    _setup_glassfish_rules()
+    
+  _closing_chains() 
+  _setup_postrouting()
+  _iptables_save()
+        
 def iptables(args):
   general.shell_exec("/sbin/iptables " + args)
+
+#
+# Private members
+#
+
+def _deny_all():
+  app.print_verbose("Deny all.")
+  iptables("-P INPUT DROP")
+  iptables("-P FORWARD DROP")
+  iptables("-P OUTPUT DROP")
+  
+def _create_chains():
+  app.print_verbose("Create bad_tcp_packets chain.")
+  iptables("-N bad_tcp_packets")
+  iptables("-A bad_tcp_packets -p tcp --tcp-flags SYN,ACK SYN,ACK -m state --state NEW -j REJECT --reject-with tcp-reset")
+  iptables("-A bad_tcp_packets -p tcp ! --syn -m state --state NEW -j LOG --log-prefix \"IPT: New not syn:\"")
+  iptables("-A bad_tcp_packets -p tcp ! --syn -m state --state NEW -j DROP")
+
+  app.print_verbose("Create allowed chain.")
+  iptables("-N allowed")
+  iptables("-A allowed -p TCP --syn -j ACCEPT")
+  iptables("-A allowed -p TCP -m state --state ESTABLISHED,RELATED -j ACCEPT")
+  iptables("-A allowed -p TCP -j DROP")
+
+  app.print_verbose("Create ICMP rules.")
+  iptables("-N icmp_packets")
+  iptables("-A icmp_packets -p ICMP -s 0/0 --icmp-type echo-request -j ACCEPT")
+  iptables("-A icmp_packets -p ICMP -s 0/0 --icmp-type echo-reply -j ACCEPT")
+  iptables("-A icmp_packets -p ICMP -s 0/0 --icmp-type destination-unreachable -j ACCEPT")
+  iptables("-A icmp_packets -p ICMP -s 0/0 --icmp-type source-quench -j ACCEPT")
+  iptables("-A icmp_packets -p ICMP -s 0/0 --icmp-type time-exceeded -j ACCEPT")
+  iptables("-A icmp_packets -p ICMP -s 0/0 --icmp-type parameter-problem -j ACCEPT")
+  
+def _setup_general_rules():
+  app.print_verbose("Bad TCP packets we don't want.")
+  iptables("-A INPUT   -p tcp -j bad_tcp_packets")
+  iptables("-A OUTPUT  -p tcp -j bad_tcp_packets")
+  iptables("-A FORWARD -p tcp -j bad_tcp_packets")
+
+  app.print_verbose("Standard icmp_packets from anywhere.")
+  iptables("-A INPUT  -p ICMP -j icmp_packets")
+  iptables("-A OUTPUT -p ICMP -j icmp_packets")
+  
+  app.print_verbose("From Localhost interface to Localhost IP's.")
+  iptables("-A INPUT -p ALL -i lo -s 127.0.0.1 -j ACCEPT")
+  
+def _setup_ssh_rules():
+  '''
+  Can SSH to this and any other computer internal and external.
+  
+  '''
+  app.print_verbose("Setup ssh INPUT/OUTPUT rule.")
+  ssh_ports="22,34"
+  iptables("-A INPUT -p tcp  -m multiport --dports " + ssh_ports + " -j allowed")
+  iptables("-A OUTPUT -p tcp -m multiport --dports " + ssh_ports + " -j ACCEPT")
+  
+def _setup_mysql_rules():
+  app.print_verbose("Setup mysql input rule.")
+  mysql_ports="3306"
+  iptables("-A INPUT -p TCP -m multiport --dports " + mysql_ports + " -j allowed")
+  
+def _setup_glassfish_rules():
+  app.print_verbose("Setup glassfish input rule.")
+  glassfish_ports="6048,6080,6081,7048,7080,7081"
+  iptables("-A INPUT -p TCP -m multiport --dports " + glassfish_ports + " -j allowed")
+
+def _closing_chains():
+  app.print_verbose("Allow all established and related packets incoming from anywhere.")
+  iptables("-A INPUT -p ALL -m state --state ESTABLISHED,RELATED -j ACCEPT")
+  iptables("-A OUTPUT -p ALL -m state --state ESTABLISHED,RELATED -j ACCEPT")
+  iptables("-A FORWARD -p ALL -m state --state ESTABLISHED,RELATED -j ACCEPT")
+
+  app.print_verbose("Log weird packets that don't match the above.")
+  iptables("-A INPUT -m limit --limit 3/minute --limit-burst 3 -j LOG --log-level DEBUG --log-prefix \"IPT: INPUT packet died: \"")
+  iptables("-A OUTPUT -m limit --limit 3/minute --limit-burst 3 -j LOG --log-level DEBUG --log-prefix \"IPT: OUTPUT packet died: \"")
+  iptables("-A FORWARD -m limit --limit 3/minute --limit-burst 3 -j LOG --log-level DEBUG --log-prefix \"IPT: FORWARD packet died: \"")
+  
+def _setup_postrouting():  
+  app.print_verbose("Enable simple IP Forwarding and Network Address Translation.")
+  #iptables("-t nat -A POSTROUTING -o $INET_IFACE -j SNAT --to-source $INET_IP")
+  #iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+  
+def _iptables_save():  
+  app.print_verbose("Save current iptables rules to /etc/iptables.")
+  general.shell_exec("/sbin/iptables-save ")
