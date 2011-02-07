3389447bcc8b5248608dcbdab0346df6c9173056
Daniel Lindh <daniel@cybercow.se>
2011-02-07 19:54:13 +0100

    Working draft of installation of httpd and modsecurity.  Changes to be committed: 	modified:   ../bin/public/installHttpd.py 	new file:   httpd/conf.d/001-common.conf 	new file:   httpd/conf.d/002-ssl.conf 	new file:   httpd/conf.d/003-modsecurity.conf 	new file:   httpd/conf/httpd.conf 	new file:   httpd/html/favicon.ico 	new file:   httpd/html/sec/dead.html 	new file:   httpd/html/sec/index.html 	new file:   httpd/modsecurity.d/modsecurity_crs_10_config.conf 	new file:   httpd/modsecurity.d/modsecurity_crs_10_config_allow_url.data 	new file:   httpd/modsecurity.d/modsecurity_fosh_10_custom.conf

--- bin/public/installHttpd.py
+++ bin/public/installHttpd.py
@@ -24,6 +24,8 @@ TODO: Verify that http://httpd.apache.org/docs/2.2/mod/worker.html is active and
 TODO: mod_sec SecChrootDir /chroot/apache
 TODO:  Read the mod_ssl User Manual for more details.
 TODO: Disable DocumentRoot in httpd.conf when using virtualhost.
+TODO: Add 404 page, mod_sec are redirecting to 404
+TODO: When modsec fails, execute a shell script that looks for info about the attacker???
 
 '''
 
@@ -53,13 +55,15 @@ MODSEC_REPO_URL = "http://www.modsecurity.org/download/" + MODSEC_INSTALL_FILE +
 MODSEC_ASC_FILE = MODSEC_INSTALL_FILE + ".tar.gz.asc"
 MODSEC_ASC_REPO_URL = MODSEC_REPO_URL + ".asc"
 
+MODSEC_RULES_FILE = "modsecurity-crs_2.1.1"
+
 def build_commands(commands):
   '''
   Defines the commands that can be executed through the fosh.py shell script.
 
   '''
   commands.add("install-httpd", install_httpd, help="Install apache webbserver on the current server.")
-  commands.add("uninstall_httpd", uninstall_httpd, help="remove apache webbserver on the current server.")
+  commands.add("uninstall-httpd", uninstall_httpd, help="remove apache webbserver on the current server.")
 
 def install_httpd(args):
   '''
@@ -70,27 +74,48 @@ def install_httpd(args):
   version_obj = version.Version("Installhttpd", SCRIPT_VERSION)
   version_obj.check_executed()
 
-  #_install_httpd()
-
+  _install_httpd()
   _install_mod_security()
-
-  #_install_farepayment()
-  #_set_file_permissions()
+  _update_modsec_rules()
+  set_file_permissions()
 
   general.shell_exec("/etc/init.d/httpd restart")
 
   #TODO version_obj.mark_executed()
 
+def set_file_permissions():
+  general.shell_exec("chmod 644 /etc/httpd/conf.d/*")
+  general.shell_exec("chcon system_u:object_r:httpd_config_t:s0 /etc/httpd/conf.d/*")
+
+  general.shell_exec("chown -R root:root /etc/httpd/modsecurity.d")
+  general.shell_exec("chmod 755 /etc/httpd/modsecurity.d")
+  general.shell_exec("chcon -R system_u:object_r:httpd_config_t:s0 /etc/httpd/modsecurity.d")
+
+def uninstall_httpd(args):
+  '''
+  Uninstal apache httpd.
+
+  '''
+  if os.path.exists('/etc/httpd/conf/httpd.conf'):
+    # Apache httpd
+    general.shell_exec("yum -y erase httpd apr apr-util postgresql-libs mod_ssl distcache")
+
+    #copy config files
+    general.shell_exec("rm -rf /etc/httpd/")
+    general.shell_exec("rm -rf /var/www/html")
+    general.shell_exec("rm -r /usr/lib64/httpd/modules/mod_security2.so")
+
 def _install_httpd():
   # Install yum packages for apache httpd
   if (not os.path.exists('/etc/httpd/conf/httpd.conf')):
-    general.shell_exec("yum -y install httpd")
+    general.shell_exec("yum -y install httpd mod_ssl")
     general.shell_exec("chkconfig httpd on")
 
   # Copy config files
   general.shell_exec("cp " + app.FOSH_PATH + "var/httpd/conf/httpd.conf /etc/httpd/conf/")
   general.shell_exec("cp " + app.FOSH_PATH + "var/httpd/conf.d/001-common.conf /etc/httpd/conf.d/")
-  general.shell_exec("cp " + app.FOSH_PATH + "var/httpd/conf.d/002-ssl.conf /etc/httpd/conf.d/")
+  general.shell_exec("cp " + app.FOSH_PATH + "var/httpd/conf.d/002-ssl.conf /etc/httpd/conf.d/ssl.conf")
+  general.shell_exec("mv /etc/httpd/conf.d/ssl.conf /etc/httpd/conf.d/002-ssl.conf")
 
   # Secure httpd.conf
   general.set_config_property("/etc/httpd/conf/httpd.conf", '^ServerAdmin.*', 'ServerAdmin ' + app.SERVER_ADMIN_EMAIL)
@@ -100,11 +125,11 @@ def _install_httpd():
   general.shell_exec("rm -f /etc/httpd/conf.d/welcome.conf")
 
   # Install web pages
-  general.shell_exec("cp -r " + app.FOSH_PATH + "var/httpd/html/modsecurity /var/www/html/")
+  general.shell_exec("cp -r " + app.FOSH_PATH + "var/httpd/html/* /var/www/html/")
 
 def _install_mod_security():
   if (not os.access("/usr/lib64/httpd/modules/mod_security2.so", os.F_OK)):
-    general.shell_exec("yum -y install httpd-devel mod_ssl make gcc pcre pcre-devel libxml2 libxml2-devel")
+    general.shell_exec("yum -y install httpd-devel apr apr-util pcre libxml2 make gcc pcre-devel libxml2-devel")
 
     # Downloading and verify the pgp key for modsec.
     general.download_file(MODSEC_REPO_URL)
@@ -126,40 +151,29 @@ def _install_mod_security():
     general.shell_exec("chmod 755 /usr/lib64/httpd/modules/mod_security2.so")
     general.shell_exec("chcon system_u:object_r:httpd_modules_t:s0 /usr/lib64/httpd/modules/mod_security2.so")
 
+    # Remove needed packages for installation of modsec.
+    # TODO: See if their is any other dependencies to thease packages.
+    general.shell_exec("yum -y erase httpd-devel apr-devel apr-util-devel "+
+      "cpp cyrus-sasl-devel db4-devel expat-devel glibc-devel " +
+      "glibc-headers kernel-headers libgomp openldap-devel pkgconfig pcre-devel " +
+      "libxml2-devel zlib-devel")
+
   # Install mode-sec config files.
   general.shell_exec("cp " + app.FOSH_PATH + "var/httpd/conf.d/003-modsecurity.conf /etc/httpd/conf.d/")
-  general.shell_exec("cp -R " + app.FOSH_PATH + "var/httpd/modsecurity.d /etc/httpd/")
 
+def _update_modsec_rules():
+  general.download_file("http://sourceforge.net/projects/mod-security/files/modsecurity-crs/0-CURRENT/" + MODSEC_RULES_FILE + ".tar.gz/download")
+  general.download_file("http://sourceforge.net/projects/mod-security/files/modsecurity-crs/0-CURRENT/" + MODSEC_RULES_FILE + ".tar.gz.asc/download")
 
-  # Remove needed packages for installation of modsec.
-  # TODO: See if their is any other dependencies to thease packages.
-  general.shell_exec("yum -y erase httpd-devel mod_ssl apr-devel apr-util-devel "+
-    "cpp cyrus-sasl-devel db4-devel distcache expat-devel glibc-devel " +
-    "glibc-headers kernel-headers libgomp openldap-devel pkgconfig pcre pcre-devel " +
-    "libxml2 libxml2-devel zlib-devel")
+  os.chdir(app.INSTALL_DIR)
+  #general.shell_exec("gpg --keyserver keyserver.ubuntu.com --recv-keys 4EF28948")
+  signature = general.shell_exec("gpg " + MODSEC_RULES_FILE + ".tar.gz.asc")
+  if (r'Good signature from "Ryan Barnett (OWASP Core Rule Set Project Leader) <rbarnett@trustwave.com>"' not in signature):
+    raise Exception("Invalid signature.")
 
-  #startar om apache
-  general.shell_exec("/etc/init.d/httpd restart")
+  general.shell_exec("rm -fR /etc/httpd/modsecurity.d")
+  general.shell_exec("tar zxvf " + MODSEC_RULES_FILE + ".tar.gz -C /etc/httpd")
+  general.shell_exec("mv /etc/httpd/" + MODSEC_RULES_FILE + " /etc/httpd/modsecurity.d")
 
-def _install_farepayment():
-  general.shell_exec("cp -R " + FOSH_PATH + "var/httpd/ssl /etc/httpd/")
-  general.shell_exec("cp -R " + FOSH_PATH + "var/httpd/html /var/www/")
-
-def _set_file_permissions():
-  general.shell_exec("chmod 644 /etc/httpd/conf.d/*")
-  general.shell_exec("chcon system_u:object_r:httpd_config_t:s0 /etc/httpd/conf.d/*")
-
-
-def uninstall_httpd(args):
-  '''
-  Uninstal apache httpd.
-
-  '''
-  if os.path.exists('/etc/httpd/conf/httpd.conf'):
-    # Apache httpd
-    general.shell_exec("yum -y erase httpd apr apr-util postgresql-libs")
-
-    #copy config files
-    general.shell_exec("rm -rf /etc/httpd/")
-    general.shell_exec("rm -rf /var/www/html")
-    general.shell_exec("rm -r /usr/lib64/httpd/modules/mod_security2.so")
+  # Install customized rules.
+  general.shell_exec("cp " + app.FOSH_PATH + "var/httpd/modsecurity.d/* /etc/httpd/modsecurity.d")
