519575fcac37178a670e149c350f78c2c9af37f6
Daniel Lindh <daniel@cybercow.se>
2011-03-19 16:51:58 +0100

    Fixed parser error in strig.py

--- bin/common/string.py
+++ bin/common/string.py
@@ -14,10 +14,10 @@ __version__ = "1.0.0"
 __status__ = "Production"
 
 def reverse_ip(str):
+	'''Reveverse in ip from 1.2.3.4 to 4.3.2.1'''
 	reverse_str=""
 	for num in str.split("."):
 		if (reverse_str):
 			reverse_str = "." + reverse_str
 		reverse_str = num + reverse_str
 	return reverse_str
-print reverse_string("1.2.3.4")
