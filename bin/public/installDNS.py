c992f06e23d1ad1d34adbb70d40c28861f0c6d97
root <root@fo-tp-mahetst.(none)>
2011-05-04 15:45:20 +0200

    Updated dns installation and new nagios server install

--- bin/public/installDNS.py
+++ bin/public/installDNS.py
@@ -47,7 +47,8 @@ Fixing upp so that serial number are working be adding one and add it to the tam
 zone files.
 '''
 
-def add_serial(name):
+def add_serial(name,SYCO_PATH):
+  p = re.compile('[\s]*([\d]*)[\s]*[;][\s]*Serial')
   os.system("mv "+ SYCO_PATH +"var/dns/"+name+".zone /tmp/"+name+".zone")
   o = open(SYCO_PATH +"var/dns/"+name+".zone","w") #open for append
   for line in open("/tmp/"+name+".zone"):
@@ -69,8 +70,7 @@ def install_dns(args):
       '''
       This is not loaded now
       '''
-
-    app.print_verbose("DNS Master is already installed")
+      app.print_verbose("DNS Master is already installed")
 
   else:
 
@@ -106,10 +106,9 @@ def install_dns(args):
   localnet = config.get('config', 'localnet')
   data_center = config.get('config', 'data_center')
   #role =  config.get('config','role')
+
   role  =str(args[1])
-
-
-
+  
   '''
   Depending if the server is an master then new rndc keys are genertaed if now old are done.
   If the server is slave the keys have to bee fetch from the master server.
@@ -125,26 +124,12 @@ def install_dns(args):
       else:
           os.chdir("/var/named/chroot/etc")
           os.system("scp root@"+ipmaster+":/var/named/chroot/etc/rndc_new.key ." )
+         
 
 
+  def generate_zone(location,SYCO_PATH):
 
-
-'''
-Zone Generting script
-This is tha master zone geration script.
-It will create zoned files with records from the zone file.
-1. first it will go trow the zone.cfg and locate any domains in that file.
-2. then it well read the file with the same name as the zone exempel.com
-3. The script will generat 2 zone files one for internal network and onw for external.
-The script will search in the zone file for any internal anteris if none is found then
-the external will be used both for internal network and external
-'''
-  def generate_zone(location):
-
-'''
-Setting views right
-'''
-
+     p = re.compile('[\s]*([\d]*)[\s]*[;][\s]*Serial')
      if location == "internal":
           o = open("/var/named/chroot/etc/named.conf","a") #open for append
           o.write("view 'internt' {\n")
@@ -156,10 +141,10 @@ Setting views right
           o.write("match-clients { any; };\n")
           o.close()
 
-'''
-Getting records from zone files
-and creating zone file for records
-'''
+     '''
+     Getting records from zone files
+     and creating zone file for records
+     '''
 
 
      for zone in config.options('zone'):
@@ -167,28 +152,29 @@ and creating zone file for records
                 config_zone.read(SYCO_PATH + 'var/dns/'+zone)
                 print zone
                 
-'''
-Crating zone file and setting right settings form zone.cfg file
+                '''
+                Crating zone file and setting right settings form zone.cfg file
 
-'''
+                '''
                 o = open("/var/named/chroot/var/named/data/" +location+"." + zone + ".zone","w") #open for append
                 for line in open(SYCO_PATH + "var/dns/template.zone"):
                     line = line.replace("$IPMASTER$",ipmaster)
                     line = line.replace("$IPSLAVE$",ipslave)
                     line = line.replace("$NAMEZONE$",zone)
                     serial = p.findall (line)
+                    print line
                     if len(serial) > 0:
                         line = str(int(serial[0])+1)+"   ;   Serial\n"
                     o.write(line + "\n")
-
+                    
 
                  #Wrinting out arecord to zone file
                 if location == "internal":
                     
-'''
-Getting internal network address if thy are any else go back to use external address
-Generating A record from domain file and adding them to zone file.
-'''
+                    '''
+                    Getting internal network address if thy are any else go back to use external address
+                    Generating A record from domain file and adding them to zone file.
+                    '''
                     try:
                         config_zone.options("internal_"+zone+"_arecords")
                     except ConfigParser.NoSectionError:
@@ -206,10 +192,10 @@ Generating A record from domain file and adding them to zone file.
                          for option in config_zone.options("internal_"+zone+"_arecords"):
                             o.write (option + "." + zone+ "."+ "     IN     A    "+ config_zone.get("internal_"+zone+"_arecords",option)+" \n")
                             print option+"."+ zone+"."+ "A"+ config_zone.get("internal_"+zone+"_arecords",option)+"."
-'''
-If domain is the same as local domain
-Gett all ip from local servers and add them to records.
-'''
+                            '''
+                            If domain is the same as local domain
+                            Gett all ip from local servers and add them to records.
+                            '''
 
                          if zone == app.get_domain():
                             servers = app.get_servers()
@@ -217,12 +203,12 @@ Gett all ip from local servers and add them to records.
                                 o.write (server + "." + zone+ "."+ "     IN     A    "+app.get_ip(server) +" \n")
                                 print server + app.get_ip(server)
 
-'''
-Getting all Cnames from domain file
-If there exist any names for internal network then they are used for inernal viem
-Else external names are used.
-Cnames are the added to file
-'''
+                    '''
+                    Getting all Cnames from domain file
+                    If there exist any names for internal network then they are used for inernal viem
+                    Else external names are used.
+                    Cnames are the added to file
+                    '''
                     try:
                         config_zone.options("internal_"+zone+"_cname")
                     except ConfigParser.NoSectionError:
@@ -244,21 +230,15 @@ Cnames are the added to file
                        o.write (option + "." + zone+ "."+ "     IN     A    "+ config_zone.get(zone+"_arecords",option)+" \n")
                        print option+"."+ zone+"."+ "A"+ config_zone.get(zone+"_arecords",option)+"."
 
-                 if zone == app.get_domain():
-                    servers = app.get_servers()
-                    for server in servers:
-                        o.write (server + "." + zone+ "."+ "     IN     A    "+app.get_ip(server) +" \n")
-                        print server + app.get_ip(server)
-
                  for option in config_zone.options(zone+"_cname"):
                         out= str(option)+ "     IN    CNAME   "+ str(config_zone.get(zone+"_cname",option))+"\n"
                         out2 = out.replace('$DATA_CENTER$',data_center)
                         o.write(out2)
                         print out2
 		 o.close()
-'''
-Creating zone revers file for recursive getting if domain names.
-'''
+                '''
+                Creating zone revers file for recursive getting if domain names.
+                '''
                 o = open("/var/named/chroot/var/named/data/" + location +"."+ rzone + ".zone","w") #open for append
                 for line in open(SYCO_PATH + "var/dns/recursiv-template.zone"):
                         line = line.replace("$IPMASTER$",ipmaster[::-1])
@@ -271,9 +251,9 @@ Creating zone revers file for recursive getting if domain names.
                         o.write(line + "\n")
                 o.close()
 
-'''
-Adding the new zreated zone files to named.com to be used
-'''
+                '''
+                Adding the new zreated zone files to named.com to be used
+                '''
 
                 o = open("/var/named/chroot/etc/named.conf","a") #open for append
                 for line in open(SYCO_PATH + "var/dns/"+role+"-zone.conf"):
@@ -284,9 +264,9 @@ Adding the new zreated zone files to named.com to be used
                     line = line.replace("$LOCATION$" ,location)
                     o.write(line + "\n")
                 o.close()
-'''
-Adding differin view to the config file
-'''
+     '''
+     Adding differin view to the config file
+     '''
      if location == "internal":
           o = open("/var/named/chroot/etc/named.conf","a") #open for append
           o.write("}; \n")
@@ -295,45 +275,46 @@ Adding differin view to the config file
           o = open("/var/named/chroot/etc/named.conf","a") #open for append
           o.write("};\n")
           o.close()
-'''
-Getting namd.conf tamplate and generting new file with right config.
-'''
+          '''
+          Getting namd.conf tamplate and generting new file with right config.
+          '''
 
-
+  '''
+  Setting upp named.conf with right settings
+  '''
   o = open("/var/named/chroot/etc/named.conf","a") #open for append
   for line in open(SYCO_PATH + "var/dns/"+role+"-named.conf"):
-        line = line.replace("$IPSLAVE$",ipslave)
-        line = line.replace("$IPMASTER$",ipmaster)
-        line = line.replace("$RANGE$",dnsrange)
-        line = line.replace("$FORWARD1$",forward1)
-        line = line.replace("$FORWARD2$",forward2)
-        line = line.replace("$LOCALNET$",localnet)
-        line = line.replace("$DOMAIN$",app.get_domain())
-        o.write(line)
-  o.close()    
+     line = line.replace("$IPSLAVE$",ipslave)
+     line = line.replace("$IPMASTER$",ipmaster)
+     line = line.replace("$RANGE$",dnsrange)
+     line = line.replace("$FORWARD1$",forward1)
+     line = line.replace("$FORWARD2$",forward2)
+     line = line.replace("$LOCALNET$",localnet)
+     line = line.replace("$DOMAIN$",app.get_domain())
+     o.write(line)
+  o.close()
+  '''
+  Chnagin order if ip to match recusrsive lookup
+  '''
+     
+ 
+  '''
+  Generating the zone files
+  IMPORTAND that  internal is first
+  '''
+  generate_zone("internal",SYCO_PATH)
+  generate_zone("external",SYCO_PATH)
+  '''
+  Adding serial number to template
+  '''
 
- '''
- Chnagin order if ip to match recusrsive lookup
- '''
-  p = re.compile('[\s]*([\d]*)[\s]*[;][\s]*Serial' )
-
-'''
-Generating the zone files
-IMPORTAND that  internal is first
-'''
-  generate_zone("internal")
-  generate_zone("external")
- '''
- Adding serial number to template
- '''
-
-  add_serial("recursiv-template")
-  add_serial("template")
+  add_serial("recursiv-template",SYCO_PATH)
+  add_serial("template",SYCO_PATH)
 
 
   
   '''
- Restaring DNS server for action to be loaded
+  Restaring DNS server for action to be loaded
   '''
   general.shell_exec("/etc/init.d/named restart")
  
