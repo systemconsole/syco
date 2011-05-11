df34209d96d7572ca57ab35fa68eab2cd9e496d9
arlukin <daniel@cybercow.se>
2011-05-11 16:32:12 +0200

    Using NFS4 aprouch of exports.

--- bin/common/nfs.py
+++ bin/common/nfs.py
@@ -35,7 +35,7 @@ def add_export(name, path):
   general.set_config_property("/etc/exports", "^/exports/" + name + ".*$", "/exports/" + name + " *(rw,sync,nohide)")
 
   # Only needed once, but is dublicate here.
-  general.set_config_property("/etc/exports", r"/exports *(ro,fsid=0)", r"/exports *(ro,fsid=0)")
+  general.set_config_property("/etc/exports", "\/exports \*\(ro\,fsid\=0\)", "/exports *(ro,fsid=0)")
 
   # TODO : Using thease mount parameters?
   #general.set_config_property("/etc/exports", "^" + name + ".*$", name + " *(rw,sync,nohide,insecure,root_squash,no_subtree_check,fsid=0)")
