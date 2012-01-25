3136eef1d9d475cda292f12a8d5422f2f8b97033
arlukin <daniel@cybercow.se>
2012-01-25 15:55:59 +0100

    Updated java version in glassfish installation to 6u30

--- bin/public/installGlassfish31.py
+++ bin/public/installGlassfish31.py
@@ -53,8 +53,8 @@ GLASSFISH_DOMAINS_PATH = GLASSFISH_INSTALL_PATH + "glassfish/domains/"
 JAVA_TEMP_PATH = GLASSFISH_INSTALL_PATH + "tmp"
 
 # http://www.oracle.com/technetwork/java/javase/downloads/index.html
-JDK_INSTALL_FILE = "jdk-6u29-linux-x64-rpm.bin"
-JDK_REPO_URL     = "http://download.oracle.com/otn-pub/java/jdk/6u29-b11/" + JDK_INSTALL_FILE
+JDK_INSTALL_FILE = "jdk-6u30-linux-x64-rpm.bin"
+JDK_REPO_URL     = "http://download.oracle.com/otn-pub/java/jdk/6u30-b12/" + JDK_INSTALL_FILE
 JDK_INSTALL_PATH = "/usr/java/jdk1.6.0_29"
 
 # Mysql Connector
