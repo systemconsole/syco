05f989925393dbf760bc6624bd19ecbf50c23788
David Skeppstedt <david@micronova.se>
2014-04-28 11:31:44 +0200

    Updates
    Added the use of constants and a hint of dependency files. And examples
    to the config files in the example syco-provate included in syco.

---
+++ bin/public/installRedis.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+'''
+This script will install Redis on the targeted server.
+
+This script is dependent on the following config files in syco-private for this script to work.
+  var/redis/redis.conf
+  var/redis/keepalived.conf
+  var/redis/check-redis
+'''
+
+__author__ = "David Skeppstedt"
+__copyright__ = "Copyright 2014, Fareoffice CRS AB"
+__maintainer__ = "David Skeppstedt"
+__email__ = "davske@fareoffice.com"
+__credits__ = ["Daniel Lindh, Mattias Hemmingsson, Kristoffer Borgstrom"]
+__license__ = "???"
+__version__ = "2.4.10"
+__status__ = "Production"
+
+import os
+import iptables
+import socket
+import install
+import app
+import version
+
+script_version = 1
+
+#Setting paths
+SYCO_FO_PATH = app.SYCO_PATH + "usr/syco-private/"
+REDIS_SCRIPT_DIR = "/usr/bin/"
+REDIS_CONF_DIR = "/etc/"
+KEEPALIVED_CONF_DIR = "/etc/keepalived/"
+
+def build_commands(commands):
+  '''
+  Defines the commands that can be executed through the syco.py shell script.
+  '''
+
+  commands.add("install-redis", install_redis, help="Install Redis on the server.")
+  commands.add("uninstall-redis", uninstall_redis, help="Uninstall Redis from the server.")
+
+def install_redis(args):
+  '''
+  Redis Installation
+  '''
+
+  app.print_verbose("Install Redis version: %d" % script_version)
+  version_obj = version.Version("InstallRedis", script_version)
+  version_obj.check_executed()
+
+  os.chdir("/")
+  install.package("tcl redis keepalived")
+
+  # Add iptables rules.
+  iptables.iptables("-A syco_input -p tcp -m multiport --dports 6379 -j allowed_tcp")
+  iptables.iptables("-A syco_output -p tcp -m multiport --dports 6379 -j allowed_tcp")
+  iptables.iptables("-D multicast_packets -s 224.0.0.0/4 -j DROP")
+  iptables.iptables("-D multicast_packets -d 224.0.0.0/4 -j DROP")
+  iptables.iptables("-A multicast_packets -d 224.0.0.0/8 -j ACCEPT")
+  iptables.iptables("-A multicast_packets -s 224.0.0.0/8 -j ACCEPT")
+  iptables.iptables("-A syco_input -p 112 -i eth1 -j ACCEPT")
+  iptables.iptables("-A syco_output -p 112 -o eth1 -j ACCEPT")
+  iptables.save()
+
+  #Configure Redis
+
+  x("echo 'vm.overcommit_memory = 1' >> /etc/sysctl.conf")
+  x("mv {0}redis.conf {1}org.redis.conf".format(REDIS_CONF_DIR, REDIS_CONF_DIR))
+  x("cp {0}var/redis/redis.conf {1}redis.conf".format(SYCO_FO_PATH, REDIS_CONF_DIR))
+  x("cp {0}var/redis/redis-check {1}redis-check".format(SYCO_FO_PATH, REDIS_SCRIPT_DIR))
+  x("chmod 755 {0}redis-check".format(REDIS_SCRIPT_DIR))
+
+  # Configure Keepalived
+  x("echo 'net.ipv4.ip_nonlocal_bind = 1' >> /etc/sysctl.conf")
+  x("mv {0}keepalived.conf {1}org.keepalived.conf".format(KEEPALIVED_CONF_DIR, KEEPALIVED_CONF_DIR))
+  x("cp {0}var/redis/keepalived.conf {1}keepalived.conf".format(KEEPALIVED_CONF_DIR, KEEPALIVED_CONF_DIR))
+
+  x("sed -i s/REDIS%%IDDCB%%/{0}}/g {1}keepalived.conf".format(socket.gethostname().upper(), KEEPALIVED_CONF_DIR))
+  x("sed -i s/REDIS%%IDDCS%%/{0}}/g {1}keepalived.conf".format(socket.gethostname().lower(), KEEPALIVED_CONF_DIR))
+
+  # Start the services.
+  x("sysctl -p")
+  x("/sbin/chkconfig keepalived on")
+  x("service keepalived restart")
+  x("/sbin/chkconfig redis on")
+  x("service redis restart")
+
+  version_obj.mark_executed()
+
+def uninstall_redis(args):
+  '''
+  Remove Redis from the server
+  '''
+  #return
+  app.print_verbose("Uninstall Redis")
+
+  os.chdir("/")
+  x("/sbin/chkconfig redis off")
+  x("service redis stop")
+  x("/sbin/chkconfig keepalived off")
+  x("service keepalived stop")
+  x("yum -y remove redis keepalived")
+  x("rm -rf {0}redis.conf".format(REDIS_CONF_DIR))
+  x("rm -rf {0}redis.conf.rpmsave".format(REDIS_CONF_DIR))
+  x("rm -rf {0}*".format(KEEPALIVED_CONF_DIR))
+  iptables.iptables("-D syco_input -p tcp -m multiport --dports 6379 -j allowed_tcp")
+  iptables.iptables("-D syco_output -p tcp -m multiport --dports 6379 -j allowed_tcp")
+  iptables.iptables("-D multicast_packets -d 224.0.0.0/8 -j ACCEPT")
+  iptables.iptables("-D multicast_packets -s 224.0.0.0/8 -j ACCEPT")
+  iptables.iptables("-D syco_input -p 112 -i eth1 -j ACCEPT")
+  iptables.iptables("-D syco_output -p 112 -o eth1 -j ACCEPT")
+  iptables.iptables("-A multicast_packets -s 224.0.0.0/4 -j DROP")
+  iptables.iptables("-A multicast_packets -d 224.0.0.0/4 -j DROP")
+  iptables.save()
+
+'''
+End of Redis installation script.
+'''
