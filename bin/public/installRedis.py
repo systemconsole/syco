137c6e0f72c7d13d2f9467b4022edf33467354fe
David Skeppstedt <david@micronova.se>
2014-04-29 16:37:44 +0200

    Splitted the installation into chunks.
    Splitted the Redis installation into chunks.

--- bin/public/installRedis.py
+++ bin/public/installRedis.py
@@ -6,6 +6,8 @@ This script is dependent on the following config files in syco-private for this 
   var/redis/redis.conf
   var/redis/keepalived.conf
   var/redis/check-redis
+
+It is also dependent on the EPEL repo since Redis is not available on CentOS official Repo.
 '''
 
 __author__ = "David Skeppstedt"
@@ -41,18 +43,73 @@ def build_commands(commands):
   commands.add("uninstall-redis", uninstall_redis, help="Uninstall Redis from the server.")
 
 def install_redis(args):
-  '''
-  Redis Installation
-  '''
-
   app.print_verbose("Install Redis version: %d" % script_version)
   version_obj = version.Version("InstallRedis", script_version)
   version_obj.check_executed()
-
   os.chdir("/")
-  install.package("tcl redis keepalived")
 
-  # Add iptables rules.
+  '''
+  Install the packages and then configure them.
+  '''
+
+  install.package("tcl redis keepalived")
+  _configure_iptables()
+  _configure_keepalived()
+  _configure_redis()
+
+  x("sysctl -p")
+
+  version_obj.mark_executed()
+
+
+def _configure_keepalived():
+  '''
+  * Keepalived needs the possibility to bind on non local adresses.
+  * It will replace the variables in the config file with the hostname.
+  * It is not enviromental dependent and can be installed on any server.
+  '''
+
+  x("echo 'net.ipv4.ip_nonlocal_bind = 1' >> /etc/sysctl.conf")
+  x("mv {0}keepalived.conf {1}org.keepalived.conf".format(KEEPALIVED_CONF_DIR, KEEPALIVED_CONF_DIR))
+  x("cp {0}var/redis/keepalived.conf {1}keepalived.conf".format(KEEPALIVED_CONF_DIR, KEEPALIVED_CONF_DIR))
+  x("sed -i s/${REDIS_SERVER_NAME_UP}/{0}/g {1}keepalived.conf".format(socket.gethostname().upper(), KEEPALIVED_CONF_DIR))
+  x("sed -i s/${REDIS_SERVER_NAME_DN}/{0}/g {1}keepalived.conf".format(socket.gethostname().lower(), KEEPALIVED_CONF_DIR))
+  _chkconfig("keepalived","on")
+  _service("keepalived","restart")
+
+
+def _configure_redis():
+  '''
+  * Redis needs to be able to overcommit memory or it will fail during replication.
+  * It does not have any enviromental specific configuration and can be installed on any server or enviroment.
+  * redis-check is the script keepalived uses in order to setup master/slave replication.
+  '''
+
+  x("echo 'vm.overcommit_memory = 1' >> /etc/sysctl.conf")
+  x("mv {0}redis.conf {1}org.redis.conf".format(REDIS_CONF_DIR, REDIS_CONF_DIR))
+  x("cp {0}var/redis/redis.conf {1}redis.conf".format(SYCO_FO_PATH, REDIS_CONF_DIR))
+  x("cp {0}var/redis/redis-check {1}redis-check".format(SYCO_FO_PATH, REDIS_SCRIPT_DIR))
+  x("chmod 755 {0}redis-check".format(REDIS_SCRIPT_DIR))
+  _chkconfig("redis","on")
+  _service("redis","restart")
+
+
+def _service(service,command):
+  x("service {0} {1}".format(service, command))
+
+
+def _chkconfig(service,command):
+  x("/sbin/chkconfig {0} {1}".format(service, command))
+
+
+def _configure_iptables(args):
+  '''
+  * Keepalived uses multicast and VRRP protocol to talk to the nodes and need to 
+    be opened. So first we remove the multicast blocks and then open them up.
+  * VRRP is known as Protocol 112 in iptables.
+  * Redis uses port 6379 and need to be opened.
+  '''
+
   iptables.iptables("-A syco_input -p tcp -m multiport --dports 6379 -j allowed_tcp")
   iptables.iptables("-A syco_output -p tcp -m multiport --dports 6379 -j allowed_tcp")
   iptables.iptables("-D multicast_packets -s 224.0.0.0/4 -j DROP")
@@ -63,30 +120,6 @@ def install_redis(args):
   iptables.iptables("-A syco_output -p 112 -o eth1 -j ACCEPT")
   iptables.save()
 
-  #Configure Redis
-
-  x("echo 'vm.overcommit_memory = 1' >> /etc/sysctl.conf")
-  x("mv {0}redis.conf {1}org.redis.conf".format(REDIS_CONF_DIR, REDIS_CONF_DIR))
-  x("cp {0}var/redis/redis.conf {1}redis.conf".format(SYCO_FO_PATH, REDIS_CONF_DIR))
-  x("cp {0}var/redis/redis-check {1}redis-check".format(SYCO_FO_PATH, REDIS_SCRIPT_DIR))
-  x("chmod 755 {0}redis-check".format(REDIS_SCRIPT_DIR))
-
-  # Configure Keepalived
-  x("echo 'net.ipv4.ip_nonlocal_bind = 1' >> /etc/sysctl.conf")
-  x("mv {0}keepalived.conf {1}org.keepalived.conf".format(KEEPALIVED_CONF_DIR, KEEPALIVED_CONF_DIR))
-  x("cp {0}var/redis/keepalived.conf {1}keepalived.conf".format(KEEPALIVED_CONF_DIR, KEEPALIVED_CONF_DIR))
-
-  x("sed -i s/REDIS%%IDDCB%%/{0}}/g {1}keepalived.conf".format(socket.gethostname().upper(), KEEPALIVED_CONF_DIR))
-  x("sed -i s/REDIS%%IDDCS%%/{0}}/g {1}keepalived.conf".format(socket.gethostname().lower(), KEEPALIVED_CONF_DIR))
-
-  # Start the services.
-  x("sysctl -p")
-  x("/sbin/chkconfig keepalived on")
-  x("service keepalived restart")
-  x("/sbin/chkconfig redis on")
-  x("service redis restart")
-
-  version_obj.mark_executed()
 
 def uninstall_redis(args):
   '''
@@ -96,14 +129,17 @@ def uninstall_redis(args):
   app.print_verbose("Uninstall Redis")
 
   os.chdir("/")
-  x("/sbin/chkconfig redis off")
-  x("service redis stop")
-  x("/sbin/chkconfig keepalived off")
-  x("service keepalived stop")
+
+  _chkconfig("redis","off")
+  _service("redis","stop")
+  _chkconfig("keepalived","on")
+  _service("keepalived","restart")
+
   x("yum -y remove redis keepalived")
   x("rm -rf {0}redis.conf".format(REDIS_CONF_DIR))
   x("rm -rf {0}redis.conf.rpmsave".format(REDIS_CONF_DIR))
   x("rm -rf {0}*".format(KEEPALIVED_CONF_DIR))
+
   iptables.iptables("-D syco_input -p tcp -m multiport --dports 6379 -j allowed_tcp")
   iptables.iptables("-D syco_output -p tcp -m multiport --dports 6379 -j allowed_tcp")
   iptables.iptables("-D multicast_packets -d 224.0.0.0/8 -j ACCEPT")
@@ -113,6 +149,7 @@ def uninstall_redis(args):
   iptables.iptables("-A multicast_packets -s 224.0.0.0/4 -j DROP")
   iptables.iptables("-A multicast_packets -d 224.0.0.0/4 -j DROP")
   iptables.save()
+
 
 '''
 End of Redis installation script.
