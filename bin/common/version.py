671f13e8d2e24bafe8631e9cc552532a79655c6e
Arlukin <daniel@cybercow.se>
2015-01-12 17:28:34 +0100

    Version also support string app_version, used by clam.

--- bin/common/version.py
+++ bin/common/version.py
@@ -12,7 +12,6 @@ Example:
     version_obj.check_executed()
 
     # Do some code, that should only be executed once on this server.
-
     version_obj.mark_executed()
   except Exception, e
     print(e)
@@ -42,30 +41,38 @@ class Version:
     hostname = socket.gethostname()
 
     command = None
-    version = None
+    script_version = None
+    app_version = None
 
     config_file_name = app.SYCO_ETC_PATH + "version.cfg"
 
-    def __init__(self, command, version):
+    def __init__(self, command, script_version, app_version=None):
         self.command = command
-        self.version = version
+        self.script_version = script_version
+        self.app_version = app_version
 
     def check_executed(self):
         """Check if the command has been executed, raise Exception otherwise."""
         if app.options.force == 0 and self.is_executed():
+            if self.app_version:
+                app_version = "app_version %s" % self.app_version
+            else:
+                app_version = None
             raise VersionException(
-                "Command " + str(self.command) + " version " + str(
-                    self.version) + " is already executed")
+                "Command: %s version %s %s is already executed" % (
+                    self.command, self.script_version, app_version
+                )
+            )
 
     def mark_executed(self):
         """Mark that a specific command/routine/script with a specific
         version has been executed on the current server."""
-        self._set_version(self.version)
+        self._set_version(self.script_version, self.app_version)
 
     def mark_uninstalled(self):
         """Mark that a specific command/routine/script with a verion 0, which means
         that the command has not been executed on the current server."""
-        self._set_version(0)
+        self._set_version(None, None)
 
     def is_executed(self):
         """Check if a specific command/routine/script with a specific
@@ -75,11 +82,16 @@ class Version:
 
         if config.has_section(self.hostname):
           if config.has_option(self.hostname, self.command):
-            if config.get(self.hostname, self.command) == self.app_version:
-                return True
+            if config.getint(self.hostname, self.command) >= self.script_version:
+                if config.has_option(self.hostname, "%s-app" % self.command):
+                    if config.get(self.hostname, "%s-app" % self.command) == self.app_version:
+                        return True
+                else:
+                    return True
+
         return False
 
-    def _set_version(self, version):
+    def _set_version(self, script_version, app_version):
         """Set version to version config file."""
         config = ConfigParser.RawConfigParser()
         config.read(self.config_file_name)
@@ -87,7 +99,15 @@ class Version:
         if not config.has_section(self.hostname):
             config.add_section(self.hostname)
 
-        config.set(self.hostname, self.command, version)
+        if script_version:
+            config.set(self.hostname, self.command, script_version)
+        else:
+            config.remove_option(self.hostname, self.command)
+
+        if app_version:
+            config.set(self.hostname, "%s-app" % self.command, app_version)
+        else:
+            config.remove_option(self.hostname, "%s-app" % self.command)
         config_file = open(self.config_file_name, 'wb')
         config.write(config_file)
         config_file.close()
