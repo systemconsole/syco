88c18437db443a8394c5102382b9d3dc048b3d45
Kribor <kristofer@libertar.se>
2015-03-18 16:40:08 +0100

    pep fix

--- lib/nagios/plugins_nrpe/check_ulimit.py
+++ lib/nagios/plugins_nrpe/check_ulimit.py
@@ -45,7 +45,7 @@ crit = int(sys.argv[3])
 limit = int(subprocess.Popen("sudo -u {0} cat /proc/self/limits | grep 'open files' | awk '{ print $4 }'".format(username), shell=True, stdout=subprocess.PIPE).stdout.read().strip())
 current = int(subprocess.Popen("sudo lsof | grep ' {0} ' | awk '{print $NF}' | sort | wc -l".format(username), shell=True, stdout=subprocess.PIPE).stdout.read().strip())
 
-percent =  int(round(((float(current) / float(limit)) * 100),0))
+percent = int(round(((float(current) / float(limit)) * 100),0))
 
 if percent >= warn:
     if percent >= crit:
