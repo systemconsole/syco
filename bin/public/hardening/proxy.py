6d3655c8c4fbc86f479596675c8f256b51f185a5
martin <martin@sysop.martin>
2015-07-14 16:19:16 +0200

    client proxy setup

--- bin/public/hardening/proxy.py
+++ bin/public/hardening/proxy.py
@@ -13,5 +13,5 @@ def install_proxy_setup():
     x("cp %shardening/proxy.sh /etc/profile.d" % app.SYCO_VAR_PATH)
     x("chmod 644 /etc/profile.d/proxy.sh")
     #Pass proxy variable when running sudo
-    proxenv='Defaults env_keep += "http_proxy"'
+    proxenv='Defaults env_keep +="http_proxy"'
     x("echo "+proxenv+" >> /etc/sudoers")
