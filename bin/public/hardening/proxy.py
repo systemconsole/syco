a496543254776957c142438508b705c72635c64a
martin <martin@sysop.martin>
2015-08-03 16:42:22 +0200

    fix for rerunning hardening

--- bin/public/hardening/proxy.py
+++ bin/public/hardening/proxy.py
@@ -10,8 +10,9 @@ from scopen import scOpen
 
 
 def install_proxy_setup():
-    x("cp %shardening/proxy.sh /etc/profile.d" % app.SYCO_VAR_PATH)
-    x("chmod 644 /etc/profile.d/proxy.sh")
-    #Pass proxy variable when running sudo
-    proxenv='Defaults env_keep +="http_proxy"'
-    x("echo "+proxenv+" >> /etc/sudoers")
+    if not os.path.exists('/etc/profile.d/proxy.sh'):
+        x("cp %shardening/proxy.sh /etc/profile.d" % app.SYCO_VAR_PATH)
+        x("chmod 644 /etc/profile.d/proxy.sh")
+        #Pass proxy variable when running sudo
+        proxenv='Defaults env_keep +="http_proxy"'
+        x("echo "+proxenv+" >> /etc/sudoers")
