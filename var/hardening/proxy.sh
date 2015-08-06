c81c4e951fadd663b214551a8fbe95a1af72231c
martin <martin@sysop.martin>
2015-08-06 14:12:32 +0200

    get proxy ip/port during hardening

--- var/hardening/proxy.sh
+++ var/hardening/proxy.sh
@@ -5,7 +5,5 @@ echo "Proxy username:"
 read -e username
 echo "password:"
 read -es password
-proxyhost=$(grep http.proxy.host /opt/syco/etc/general.cfg |cut -c18-32)
-proxyport=$(grep http.proxy.port /opt/syco/etc/general.cfg |cut -c18-22)
 export http_proxy="http://$username:$password@$proxyhost:$proxyport/"
 }
