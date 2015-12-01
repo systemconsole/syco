3af576eb00c4f218c9dc935a762a2e9f21c9f99a
nemie <pirullinen@gmail.com>
2015-12-01 15:38:18 +0100

    Added a new check for our disks and raid

--- bin/common/install.py
+++ bin/common/install.py
@@ -75,10 +75,10 @@ def hp_repo():
     x("""cat > /etc/yum.repos.d/hp.repo << EOF
 [HP-Proliant]
 name=Software Delivery Repository \$releasever - \$basearch
-baseurl=http://downloads.linux.hpe.com/SDR/repo/spp/rhel/\$releasever/\$basearch/current/
+baseurl=http://downloads.linux.hp.com/SDR/downloads/ServicePackforProLiant/RedHat/$releasever/$basearch/current/
 enabled=1
 gpgcheck=0
-gpgkey=https://downloads.linux.hpe.com/SDR/repo/spp/GPG-KEY-SPP
+gpgkey=http://downloads.linux.hp.com/SDR/downloads/ServicePackforProLiant/GPG-KEY-ProLiantSupportPack
 EOF""")
 
 
