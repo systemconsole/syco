a68b9d930fdbbe9f62f68f49c703fd19e7a8c3b8
Nemie <pirullinen@gmail.com>
2015-12-03 09:32:00 +0100

    Fixed https and link for GPG (some misspelling)

--- bin/common/install.py
+++ bin/common/install.py
@@ -75,10 +75,10 @@ def hp_repo():
     x("""cat > /etc/yum.repos.d/hp.repo << EOF
 [HP-Proliant]
 name=Software Delivery Repository \$releasever - \$basearch
-baseurl=http://downloads.linux.hp.com/SDR/downloads/ServicePackforProLiant/RedHat/$releasever/$basearch/current/
+baseurl=https://downloads.linux.hp.com/SDR/downloads/ServicePackforProLiant/RedHat/$releasever/$basearch/current/
 enabled=1
 gpgcheck=0
-gpgkey=http://downloads.linux.hp.com/SDR/downloads/ServicePackforProLiant/GPG-KEY-ProLiantSupportPack
+gpgkey=https://downloads.linux.hp.com/SDR/downloads/ServicePackforProLiant/GPG-KEY-ServicePackforProLiant
 EOF""")
 
 
