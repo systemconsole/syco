cad8c45e98eb55c8b5ecdf2c1dbf80f070a97897
kemani <martinkemani@gmail.com>
2016-05-30 11:33:09 +0200

    new url for clam 0.99.2

--- bin/public/installClam.py
+++ bin/public/installClam.py
@@ -18,7 +18,7 @@ __status__ = "Production"
 
 
 # Path to clam installation.
-CLAM_AV_URL = "https://sourceforge.net/projects/clamav/files/clamav/{0}/clamav-{0}.tar.gz/download?use_mirror=netcologne"
+CLAM_AV_URL = "http://www.clamav.net/downloads/production/clamav-{0}.tar.gz"
 
 import app
 from general import x, urlretrive
