130db51cf402da12198f22fd40fed36876381c27
nemie <pirullinen@gmail.com>
2016-12-15 13:21:12 +0100

    Fixed the broken ulimit script

--- lib/nagios/plugins_nrpe/check_ulimit.py
+++ lib/nagios/plugins_nrpe/check_ulimit.py
@@ -42,9 +42,9 @@ username = sys.argv[1]
 warn = int(sys.argv[2])
 crit = int(sys.argv[3])
 
-limit = int(subprocess.Popen("sudo -u {0} cat /proc/self/limits | grep 'open files' | awk '{ print $4 }'".format(
+limit = int(subprocess.Popen("sudo -u {0} cat /proc/self/limits | grep 'open files' | awk '{{ print $4 }}'".format(
     username), shell=True, stdout=subprocess.PIPE).stdout.read().strip())
-current = int(subprocess.Popen("sudo lsof | grep ' {0} ' | awk '{print $NF}' | sort | wc -l".format(username),
+current = int(subprocess.Popen("sudo lsof | grep ' {0} ' | awk '{{print $NF}}' | sort | wc -l".format(username),
                                shell=True, stdout=subprocess.PIPE).stdout.read().strip())
 
 percent = int(round(((float(current) / float(limit)) * 100),0))
